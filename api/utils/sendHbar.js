33ad00ac873b6d6da13515def8e0b5f3b572abee
Wilkenson <wikicanton439@gmail.com>
2025-09-26 02:15:36 -0400

    refactor: remove unused sendHbar and sendNft utility functions

--- api/utils/sendHbar.js
+++
@@ -1,4 +0,0 @@
-import { TransferTransaction } from "@hashgraph/sdk";
-import { supabase } from "./supabase";
-
-export const sendHbar = async (amount, acount, offert) => {};
--- api/utils/sendNft.js
+++
@@ -1,1 +0,0 @@
-export const sendNft = async (amount, acount) => {};
